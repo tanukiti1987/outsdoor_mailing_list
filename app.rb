22d2132c3d2216546b54cb14f35ab35f566ea1c4
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2016-08-11 14:06:32 +0900

    Add view base

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 class App < Sinatra::Base
   register Sinatra::Reloader
-  
+
   get '/' do
-    'Hello World'
+    haml :index
+  end
+
+  get %r{^/(stylesheets|javascripts)/(.*)\.(css|js)$} do
+    dir = params[:captures][0]
+    file = params[:captures][1]
+    method = params[:captures][2] == 'css' ? :scss : :coffee
+    send(method, :"assets/#{dir}/#{file}")
   end
 end
